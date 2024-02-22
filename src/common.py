a7918773ea0fc0416a351207f9518abfe563109b
Lorenzo Rutayisire <loryruta23@gmail.com>
2024-02-22 14:54:28 +0100

    Add threadpool in common.py

--- src/common.py
+++ src/common.py
@@ -1,7 +1,21 @@
 from os import path
+import multiprocess
+from concurrent.futures import ThreadPoolExecutor
+from fast_iter import FastIterator
+
+# ------------------------------------------------------------------------------------------------
+# Configuration constants
+# ------------------------------------------------------------------------------------------------
 
 BASE_DIR = path.abspath(path.dirname(__file__))
 
-DATASET_DIR = path.join(BASE_DIR, "data")
-DATASET_PATH = path.join(DATASET_DIR, "zinc.csv")
-VOCAB_PATH = path.join(DATASET_DIR, "vocab.csv")
+DATA_DIR = path.join(BASE_DIR, "data")
+DATASET_PATH = path.join(DATA_DIR, "zinc.csv")
+VOCAB_PATH = path.join(DATA_DIR, "vocab.csv")
+MOTIF_GRAPH_PATH = path.join(DATA_DIR, "motif_graph.gml")
+
+# ------------------------------------------------------------------------------------------------
+
+# process_pool = multiprocess.Pool()
+threadpool = ThreadPoolExecutor(max_workers=16)
+# fast_iterator = FastIterator(process_pool, threadpool)
